b76d4e494bc90623eefa977ee57226efc5716ff3
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2021-02-10 15:21:37 -0700

    Update images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -45,21 +45,21 @@ e e e e c e e e e e c . . . .
 . . . e e e e e . . . . . . . 
 . . e e e . e e c . . . . . . 
 `, img`
-. . . . . . 7 7 7 7 7 7 7 . . 
-. . . . . 7 7 7 7 7 7 7 7 7 . 
-. . . . . 7 7 7 7 7 7 7 7 7 . 
-. . . . 7 7 7 7 7 7 7 . 7 7 7 
-. . . 5 5 7 7 5 5 7 5 c . 7 7 
-. . . e e e e e e e 5 e c 7 7 
-. . e e 5 4 e 5 4 e e e c . . 
-. . e e 5 4 e 5 4 e e e c . . 
-. . e e e c e e e e e e c . . 
-. . . e c f c e e e e e c . . 
-. . . e e c e e e e e c . . . 
-. . . . e e e e e e c . . . . 
-. . . . . e e e e e . . . . . 
-. . . . . . e . e . . . . . . 
-. . . . e e e . e e c . . . . 
+. . . . . . . . 7 7 7 7 7 7 7 
+. . . . . . . 7 7 7 7 7 7 7 7 
+7 . . . . . . 7 7 7 7 7 7 7 7 
+7 . . . . . 7 7 7 7 7 7 7 . 7 
+7 7 . . . 5 5 7 7 5 5 7 5 c . 
+7 7 . . . e e e e e e e 5 e c 
+7 7 . . e e 5 4 e 5 4 e e e c 
+. . . . e e 5 4 e 5 4 e e e c 
+. . . . e e e c e e e e e e c 
+. . . . . e c f c e e e e e c 
+. . . . . e e c e e e e e c . 
+. . . . . . e e e e e e c . . 
+. . . . . . . e e e e e . . . 
+. . . . . . . . e . e . . . . 
+. . . . . . e e e . e e c . . 
 `];
             case "Deku link ani1":
             case "anim2":return [img`
@@ -79,21 +79,21 @@ e e e e c e e e e e c . . . .
 . . . . e e e e e . . . . . . 
 . . . e e e . e e c . . . . . 
 `, img`
-. . . . . . . . 7 7 7 . . . . 
-. . . . . . 7 7 7 7 7 7 7 . . 
-. . . . . 7 7 7 7 7 7 7 7 7 . 
-. . . . 7 7 7 7 7 7 7 . 7 7 . 
-. . . 5 5 7 7 5 5 7 5 c 7 7 . 
-. . . e e e e e e e 5 e c 7 . 
-. . e e 5 4 e 5 4 e e e c . . 
-. . e e 5 4 e 5 4 e e e c . . 
-. . e e e c e e e e e e c . . 
-. . . e c f c e e e e e c . . 
-. . . e e c e e e e e c . . . 
-. . . . e e e e e e c . . . . 
-. . . . . e e e e e . . . . . 
-. . . . . e . . . e . . . . . 
-. . . e e e . . . e e c . . . 
+. . . . . . . . . . 7 7 7 . . 
+. . . . . . . . 7 7 7 7 7 7 7 
+. . . . . . . 7 7 7 7 7 7 7 7 
+7 . . . . . 7 7 7 7 7 7 7 . 7 
+7 . . . . 5 5 7 7 5 5 7 5 c 7 
+7 . . . . e e e e e e e 5 e c 
+7 . . . e e 5 4 e 5 4 e e e c 
+. . . . e e 5 4 e 5 4 e e e c 
+. . . . e e e c e e e e e e c 
+. . . . . e c f c e e e e e c 
+. . . . . e e c e e e e e c . 
+. . . . . . e e e e e e c . . 
+. . . . . . . e e e e e . . . 
+. . . . . . . e . . . e . . . 
+. . . . . e e e . . . e e c . 
 `];
         }
         return null;
