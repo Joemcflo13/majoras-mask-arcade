a968abb83775747c9741b3c1b22327b22eacece2
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2021-02-10 15:33:07 -0700

    added a door and fixed an ani, <div style="position:relative;height:0;padding-bottom:117.6%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="https://arcade.makecode.com/---run?id=_KPgXHeV7TftP" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe></div>

--- images.g.ts
+++ images.g.ts
@@ -27,40 +27,6 @@ namespace myImages {
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "Deku link ani2":
-            case "anim1":return [img`
-. . . . . . 7 7 7 . . . . . . 
-. . . . 7 7 7 7 7 7 7 . . . . 
-. . . 7 7 7 7 7 7 7 7 . . . . 
-. . . 7 7 7 7 7 7 . . . . . . 
-. . 7 7 7 7 7 7 7 . . . . . . 
-. 5 5 7 7 5 5 7 5 c . . . . . 
-. e e e e e e e 5 e c . . . . 
-e e 5 4 e 5 4 e e e c . . . . 
-e e 5 4 e 5 4 e e e c . . . . 
-e e e e c e e e e e c . . . . 
-. e e c f c e e e e c . . . . 
-. e e e c e e e e c . . . . . 
-. . e e e e e e c . . . . . . 
-. . . e e e e e . . . . . . . 
-. . e e e . e e c . . . . . . 
-`, img`
-. . . . . . . . 7 7 7 7 7 7 7 
-. . . . . . . 7 7 7 7 7 7 7 7 
-7 . . . . . . 7 7 7 7 7 7 7 7 
-7 . . . . . 7 7 7 7 7 7 7 . 7 
-7 7 . . . 5 5 7 7 5 5 7 5 c . 
-7 7 . . . e e e e e e e 5 e c 
-7 7 . . e e 5 4 e 5 4 e e e c 
-. . . . e e 5 4 e 5 4 e e e c 
-. . . . e e e c e e e e e e c 
-. . . . . e c f c e e e e e c 
-. . . . . e e c e e e e e c . 
-. . . . . . e e e e e e c . . 
-. . . . . . . e e e e e . . . 
-. . . . . . . . e . e . . . . 
-. . . . . . e e e . e e c . . 
-`];
             case "Deku link ani1":
             case "anim2":return [img`
 . . . . . . . 7 7 7 . . . . . 
@@ -94,6 +60,40 @@ e e e e c e e e e e c . . . .
 . . . . . . . e e e e e . . . 
 . . . . . . . e . . . e . . . 
 . . . . . e e e . . . e e c . 
+`];
+            case "Deku link ani2":
+            case "anim1":return [img`
+. . . . . . 7 7 7 . . . . . . 
+. . . . 7 7 7 7 7 7 7 . . . . 
+. . . 7 7 7 7 7 7 7 7 . . . . 
+. . . 7 7 7 7 7 7 . . . . . . 
+. . 7 7 7 7 7 7 7 . . . . . . 
+. 5 5 7 7 5 5 7 5 c . . . . . 
+. e e e e e e e 5 e c . . . . 
+e e 5 4 e 5 4 e e e c . . . . 
+e e 5 4 e 5 4 e e e c . . . . 
+e e e e c e e e e e c . . . . 
+. e e c f c e e e e c . . . . 
+. e e e c e e e e c . . . . . 
+. . e e e e e e c . . . . . . 
+. . . e e e e e . . . . . . . 
+. . e e e . e e c . . . . . . 
+`, img`
+. . . . . . 7 7 7 7 7 7 7 . . 
+. . . . . 7 7 7 7 7 7 7 7 7 . 
+. . . . . 7 7 7 7 7 7 7 7 7 . 
+. . . . 7 7 7 7 7 7 7 . 7 7 7 
+. . . 5 5 7 7 5 5 7 5 c . 7 7 
+. . . e e e e e e e 5 e c 7 7 
+. . e e 5 4 e 5 4 e e e c . . 
+. . e e 5 4 e 5 4 e e e c . . 
+. . e e e c e e e e e e c . . 
+. . . e c f c e e e e e c . . 
+. . . e e c e e e e e c . . . 
+. . . . e e e e e e c . . . . 
+. . . . . e e e e e . . . . . 
+. . . . . . e . e . . . . . . 
+. . . . e e e . e e c . . . . 
 `];
         }
         return null;
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,11 +2,31 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102030307070303040101010101010101020303050603030401010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 2 2 2 2 2 . . . . . 
+. . . 2 2 2 . . 2 2 2 . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -15,15 +35,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.builtin.forestTiles10], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.builtin.forestTiles10,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +43,18 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
         }
         return null;
     })
